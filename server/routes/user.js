1649fb38ca562d130a5ee5a92a5f3be951c17789
harlesbayu <bayuanggaraharles@gmail.com>
2018-10-08 08:35:10 +0700

    update readme

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const Controller = require('../controllers/userController')
+// const isLogin = require('../middleware/isLogin')
 
 router.post('/signup', Controller.signup)
 router.post('/signin', Controller.signin)
 router.post('/signinGoogle', Controller.signinGoogle)
-// routes.put('/update/:id', update)
-// routes.delete('/delete/:id', remove)
+// router.put('/update/:id', isLogin, Controller.update)
+// router.delete('/delete/:id', isLogin, Controller.remove)
 
 module.exports = router
